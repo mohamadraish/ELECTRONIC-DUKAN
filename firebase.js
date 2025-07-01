172c06ad0a317e5e8438c64cb48b8744a716341b
mohamadraish <raeesmaknojiya829@gmail.com>
2025-07-01 14:23:42 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,1 +1,10 @@
-<!-- Firebase configuration and database connection code -->
+const firebaseConfig = {
+  apiKey: "AIzaSyDD0D3886lsY7YxpqHRpPyDGxislSHRtxs",
+  authDomain: "electronic-dukan.firebaseapp.com",
+  databaseURL: "https://electronic-dukan-default-rtdb.firebaseio.com",
+  projectId: "electronic-dukan",
+  storageBucket: "electronic-dukan.firebasestorage.app",
+  messagingSenderId: "146227825107",
+  appId: "1:146227825107:web:3c4f6faf7e541a00078726"
+};
+firebase.initializeApp(firebaseConfig);
